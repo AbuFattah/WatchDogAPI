2a41bb77bf85de4aa625d1ef90995f9387be34e9
AbuFattah <mdnahid142493@gmail.com>
2024-05-21 13:58:21 +0600

    parsed request

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import http, { IncomingMessage, ServerResponse } from "http";
+import { parse, UrlWithParsedQuery } from "url";
+import { StringDecoder } from "string_decoder";
 
 type App = {
   config?: AppConfig;
@@ -25,6 +27,31 @@ app.createServer = () => {
 };
 
 app.handleReqRes = (req, res) => {
+  const parsedUrl: UrlWithParsedQuery = parse(req.url!, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path?.replace(/^\/+|\/(?=\?)|\/$/g, "");
+
+  const method = req.method?.toLowerCase();
+  const qryStrObj = parsedUrl.query;
+  const headersObj = req.headers;
+
+  const decoder = new StringDecoder("utf-8");
+  let realData: string = "";
+
+  req.on("data", (buffer: Buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+  });
+
+  console.log(headersObj);
+
+  console.log(trimmedPath);
+  console.log(qryStrObj);
+
   res.end("Hello Pooogiess");
 };
 
